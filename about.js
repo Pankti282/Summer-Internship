e74b2ab57a31f2a84931d173159eaaf619d05a92
Pankti282 <panktipatel67890@gmail.com>
2022-07-07 13:35:37 -0400

    'initial'

--- about.js
+++ about.js
@@ -1,36 +1,107 @@
 
-// window.addEventListener('scroll', progressAll);
+
+
+
+
+window.addEventListener("scroll", progressAll);
+
+function progressAll()
+ {
+  var triggerProgress = (window.innerHeight / 2);
+  console.log(triggerProgress);
+ var progressTop = document.getElementById("about");
+
+  var progressStart = progressTop.getBoundingClientRect().top;
+  console.log(progressStart);
+  if (progressStart < triggerProgress)
+   {
+    // html
+
+    var progress = document.getElementById("progressDoneHtml");
+    progress.style.width = progress.getAttribute("data-done");
+    progress.style.opacity = 1;
+
+    // Css
+
+    var progressCss = document.getElementById("progressDoneCss");
+    progressCss.style.width = progressCss.getAttribute("data-done");
+    progressCss.style.opacity = 1;
+
+    
+
+    // Javascript
+
+    var progressJavascript = document.getElementById("progressDonejs");
+    progressJavascript.style.width = progressJavascript.getAttribute("data-done");
+    progressJavascript.style.opacity = 1;
+
+    // REACTJS
+
+    var progressReactJS = document.getElementById("progressDoneReactjs");
+    progressReactJS.style.width = progressReactJS.getAttribute("data-done");
+    progressReactJS.style.opacity = 1;
+  }
+}
+//  window.addEventListener("scroll", progressAll);
 
 // function progressAll() {
-//   const triggerProgress = window.innerHeight / 2;
-//   const progressTop = document.getElementById("container---2");
-//   const progressStart = progressTop.getBoundingClientRect().top;
-//   console.log(progressStart);
-//   if (progressStart < triggerProgress) {
+//   var height = (window.innerHeight / 8) *4;
+//   var range = document.getElementById(".container-2");
+//   var prog = range.getBoundingClientRect().top;
 
-// const progress = document.getElementById("#progressDoneHtml");
-// progress.style.width = progress.getAttribute("data-done");
+//   if (prog < height) {
 
-// const css = document.getElementById("#progressDoneCss");
-// css.style.width = css.getAttribute("data-done");
+// const progress = document.getElementById("progressDoneHtml");
+// progress.style.width = progress.getAttribute('data-done');
+// progress.style.opacity=1;
 
-// const js= document.getElementById("#progressDoneJavascript");
-// js.style.width = js.getAttribute("data-done");
+// var progress = document.getElementById("progressDoneCss");
+// progress.style.width = progress.getAttribute('data-done');
 
-// const reactjs = document.getElementById("#reactjsDoneReactjs");
-// reactjs.style.width = reactjs.getAttribute("data-done");
+// var progress = document.getElementById("progressDonejs");
+// progress.style.width = progress.getAttribute('data-done');
+
+// var progress = document.getElementById("progressDoneReactjs");
+// progress.style.width = progress.getAttribute('data-done');
 //   }
 // }
 
 
-var progress = document.getElementById('progressDoneHtml');
-progress.style.width = progress.getAttribute('data-done')
+const number=document.querySelectorAll(".number")
+const speed=100;
+number.forEach(element =>{
+    incNumber(element)
+});
+function incNumber(elem){
+    let text =+elem.innerText;
+    const value=+elem.getAttribute("data-target");
+    const inc=value/speed;
+    if(text<value){
+        elem.innerText=inc+text;
+        setTimeout(() =>{
+            incNumber(elem)
+        },20);
+    }else{
+        elem.innerText=value;
+    }
+}
 
-var progress = document.getElementById('progressDoneCss');
-progress.style.width = progress.getAttribute('data-done')
+// const Numbers =document.querySelectorAll(".number");
+// Numbers.forEach(function(number){
 
-var progress = document.getElementById('progressDoneJavascript');
-progress.style.width = progress.getAttribute('data-done')
+//     number.innerText ="20";
+//     updateNumber();
 
-var progress = document.getElementById('progressDoneReactjs');
-progress.style.width = progress.getAttribute('data-done')
+//     function updateNumber() {
+//         const target= +number.getAttribute("data-tareget");
+//         const c = +number.innerText;
+//         const increment =target/1000;
+//         if(c<target){
+//             number.innerText='${Math.ceil(c + increment)}';
+//             setTimeout(updateNumber,10);
+//         }else{
+//             number.innerText = target;
+//         }
+//         }
+    
+// });
---
+++ blog.js
@@ -0,0 +1,29 @@
+var date = new Date();
+
+document.getElementById('month').innerHTML= date.toLocaleDateString("default", {month:"long"});
+document.getElementById('date').innerHTML=date.getDate();
+document.getElementById('year').innerHTML=date.getFullYear();
+
+document.getElementById('month1').innerHTML=date.toLocaleDateString("default", {month:"long"});
+document.getElementById('date1').innerHTML=date.getDate();
+document.getElementById('year1').innerHTML=date.getFullYear();
+
+const btn1  = document.querySelector('#btn');
+const text1 = document.querySelector('.show');
+
+btn1.addEventListener('click',button);
+function button(){
+    text1.classList.toggle('hide');
+    btn1.textContent=text1.classList.contains('hide')?('Read More'):('Read Less');
+}
+
+
+const btn2 = document.querySelector('#btn-1');
+const text2 = document.querySelector('.show-1');
+btn2.addEventListener('click',display);
+function display(){
+    text2.classList.toggle('hide-1');
+    btn2.textContent=text2.classList.contains('hide-1')?('Read More'):('Read Less');
+}
+
+
